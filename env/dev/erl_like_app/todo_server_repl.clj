de67060df92963122755b795ea26828874cbcd16
andrey <andrey@cloudgears.com>
2018-05-10 18:17:39 +0300

    repl updated (PASSED)

--- env/dev/erl_like_app/todo_server_repl.clj
+++ env/dev/erl_like_app/todo_server_repl.clj
@@ -1,10 +1,20 @@
 (ns erl-like-app.todo-server-repl
   (:require
     [erl-like-app.server :as server]
-    [erl-like-app.todo-server :refer :all]))
+    [erl-like-app.todo.todo-server :refer :all]))
 
 #_(server/restart)
 
-#_(create {})
+#_(create-todo {:title "Task #1"})
+
+#_(find-todo-by-id "1")
+
+#_(terminate-todo "2")
+
+#_(delete-todo nil)
+
+#_(enumerate-active-todos)
+
+;;;;
 
 #_(get-state)
